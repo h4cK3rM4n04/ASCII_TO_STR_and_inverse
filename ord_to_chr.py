108cfde04bfd8cf5a247b13b370f8237236ebff6
h4cK3rM4n04 <eliemanoarm@gmail.com>
2023-02-16 20:46:50 +0100

    ASCII en chaine de caractères v0.1

---
+++ ord_to_chr.py
@@ -0,0 +1,10 @@
+def ord_to_chr(chaine):
+	d = []
+	for i in chaine:
+		d.append(ord(i))
+	return str(d)
+print(ord_to_chr('Ryan'))
+
+#print(ord('༆⸙༈')) '''Name FB dans 60 jours...: 	༆⸙༈  ༆⸙༈  ༆⸙༈''' 
+print(chr(178))
+print(ord('~'))
